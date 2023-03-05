0d11f87887d4fc4a7542d8eb61b3a950a476ac66
bautistamiguel966 <miguelbautista182@gmail.com>
2023-03-04 23:38:14 -0300

    Se agrego el componente proyecto

---
+++ src/main/java/com/portfolio/bma/Controller/CProyecto.java
@@ -0,0 +1,91 @@
+
+package com.portfolio.bma.Controller;
+
+import com.portfolio.bma.Dto.dtoProyecto;
+import com.portfolio.bma.Entity.Proyecto;
+import com.portfolio.bma.Security.Controller.Mensaje;
+import com.portfolio.bma.Service.SProyecto;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/proyecto")
+@CrossOrigin(origins = {"https://bmafrontend-e8b56.web.app","http://localhost:4200"})
+public class CProyecto {
+    
+    @Autowired
+    SProyecto sProyecto;
+    
+    @GetMapping("/lista")
+    public ResponseEntity<List<Proyecto>> list(){
+        List<Proyecto> list = sProyecto.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+    
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Proyecto> getById(@PathVariable("id")int id){
+        if(!sProyecto.existsById(id)){
+            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.BAD_REQUEST);
+        }
+        Proyecto proyecto = sProyecto.getOne(id).get();
+        return new ResponseEntity(proyecto, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id){
+        if(!sProyecto.existsById(id)){
+            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
+        }
+        sProyecto.delete(id);
+        return new ResponseEntity(new Mensaje("Proyecto eliminada"), HttpStatus.OK);
+    }
+    
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody dtoProyecto dtoproyecto){
+        if(StringUtils.isBlank(dtoproyecto.getNombre())){
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+        if(sProyecto.existsByNombre(dtoproyecto.getNombre())){
+            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
+        }
+        Proyecto proyecto = new Proyecto(dtoproyecto.getNombre(), dtoproyecto.getDescripcion(), dtoproyecto.getImg());
+        sProyecto.save(proyecto);
+        return new ResponseEntity(new Mensaje("Proyecto creado"), HttpStatus.OK);
+    }
+    
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id")int id, @RequestBody dtoProyecto dtoproyecto){
+        if(!sProyecto.existsById(id)){
+            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
+        }
+        if(sProyecto.existsByNombre(dtoproyecto.getNombre()) && sProyecto.getByNombre(dtoproyecto.getNombre()).get().getId() != id){
+            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
+        }
+        if(StringUtils.isBlank(dtoproyecto.getNombre())){
+            return new ResponseEntity(new Mensaje("El campo no puede estar vacio"), HttpStatus.BAD_REQUEST);
+            
+        }
+        Proyecto proyecto = sProyecto.getOne(id).get();
+        
+        proyecto.setNombre(dtoproyecto.getNombre());
+        proyecto.setDescripcion(dtoproyecto.getDescripcion());
+        proyecto.setImg(dtoproyecto.getImg());
+        
+        sProyecto.save(proyecto);
+        
+        return new ResponseEntity(new Mensaje("Proyecto actualizado"), HttpStatus.OK);
+    }
+    
+}
